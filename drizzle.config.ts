927cd2ec1637957c645b8a2c76e413da1c57707f
matéo! <6matbub@gmail.com>
2025-09-28 21:37:04 -0700

    switch env string to prod

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -5,6 +5,6 @@ export default defineConfig({
   dialect: "postgresql",
   // schema: "./drizzle/schema.ts",
   dbCredentials: {
-    url: "postgresql://neondb_owner:npg_lahdK9bx6Zpy@ep-wandering-credit-afx4zknv-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
+    url: "postgresql://neondb_owner:npg_p0MIeGDVPA4J@ep-small-bush-afr8vgj8-pooler.c-2.us-west-2.aws.neon.tech/neondb?sslmode=require&channel_binding=require",
   },
 });
