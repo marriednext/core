cf76b3c04966b7e3a354a578de9fc3d5ac3058d1
matéo! <6matbub@gmail.com>
2025-11-22 11:57:32 -0800

    EVR-171: Preserve nested CSS in development builds

--- packages/style-shelf/postcss.config.mjs
+++ packages/style-shelf/postcss.config.mjs
@@ -1,8 +1,10 @@
+const isDev = process.env.NODE_ENV === "development";
+
 export default {
   plugins: {
     "postcss-import": {},
     "postcss-mixins": {},
-    "postcss-nested": {},
+    ...(isDev ? {} : { "postcss-nested": {} }),
     autoprefixer: {},
   },
 };
